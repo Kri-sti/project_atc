4ab004bae60e657e5e225aff43f61808d7c2fd36
Kri-sti <58682386+Kri-sti@users.noreply.github.com>
2020-09-07 09:14:45 +0200

    Add files via upload

---
+++ db.php
@@ -0,0 +1,20 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "project";
+
+$conn = new mysqli($servername,$username,$password,$database);
+
+if ($conn === false) {
+  die("Couldn't" . $conn->connect_error);
+}
+
+
+
+
+
+
+$conn
+
+ ?>
